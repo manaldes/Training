57da841381bcb622b3968107c19f397544055ddf
ManaL Al-harbi <ma0910no@gmail.com>
2020-05-03 03:08:54 +0300

    Add files via upload

---
+++ CollectionViewScrollingVretical/CollectionViewScrollingVretical/Athkar.swift
@@ -0,0 +1,20 @@
+//
+//  Athkar.swift
+//  CollectionViewScrollingVretical
+//
+//  Created by Manal  harbi on 10/09/1441 AH.
+//  Copyright © 1441 ManaL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct athkari {
+    
+    
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+}
---
+++ CollectionViewScrollingVretical/CollectionViewScrollingVretical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewScrollingVretical
+//
+//  Created by Manal  harbi on 10/09/1441 AH.
+//  Copyright © 1441 ManaL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
